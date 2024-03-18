4d2ec2e582ea3bbfdb42033bb836a4646c152980
Mirko Schaefer <codewithmirko@gmail.com>
2024-03-18 18:27:49 +0100

    first day done

--- src/question.js
+++ src/question.js
@@ -1,7 +1,12 @@
 class Question {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (text, choices, answer, difficulty)
+  constructor(text, choices, answer, difficulty) {
+    this.text = text;
+    this.choices = choices;
+    this.answer = answer;
+    this.difficulty = difficulty;
+  }
 
-    // 2. shuffleChoices()
+  shuffleChoices() {
+    const randomChoices = this.choices.sort((a, b) => 0.5 - Math.random());
+  }
 }
--- src/quiz.js
+++ src/quiz.js
@@ -1,15 +1,36 @@
 class Quiz {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (questions, timeLimit, timeRemaining)
+  constructor(questions, timeLimit, timeRemaining) {
+    this.questions = questions;
+    this.timeLimit = timeLimit;
+    this.timeRemaining = timeRemaining;
+    this.correctAnswers = 0;
+    this.currentQuestionIndex = 0;
+  }
 
-    // 2. getQuestion()
-    
-    // 3. moveToNextQuestion()
+  getQuestion() {
+    return this.questions[this.currentQuestionIndex];
+  }
 
-    // 4. shuffleQuestions()
+  moveToNextQuestion() {
+    this.currentQuestionIndex += 1;
+  }
 
-    // 5. checkAnswer(answer)
+  shuffleQuestions() {
+    const randomQuestions = this.questions.sort((a, b) => 0.5 - Math.random());
+  }
 
-    // 6. hasEnded()
+  checkAnswer(answer) {
+    const currentQuestion = this.getQuestion();
+    if (currentQuestion.answer === answer) {
+      this.correctAnswers += 1;
+    }
+  }
+
+  hasEnded() {
+    if (this.currentQuestionIndex < this.questions.length) {
+      return false;
+    } else {
+      return true;
+    }
+  }
 }
